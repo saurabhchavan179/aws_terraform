7c122a3c68e01bc12653971135cac85de1873878
saurabhchavan179 <chavansourabh179@gmail.com>
2026-09-08 15:33:43 +0530

    Add Day-02 terraform

--- Day-02/variables.tf
+++ Day-02/variables.tf
@@ -30,17 +30,3 @@ variable "public_ip" {
   
 }
 
-output "instance_public_ip" {
-    description = "This is for the ec2 public ip"
-    value = aws_instance.web_apps.public_ip
-}
-
-output "instance_private_ip" {
-    description = "This is for the ec2 public ip"
-    value = aws_instance.web_apps.private_ip
-}
-
-output "instance_private_dns" {
-    description = "This is for the ec2 public ip"
-    value = aws_instance.web_apps.private_dns
-}
